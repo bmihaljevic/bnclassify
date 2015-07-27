cc18332495e9a80741d8a13ea98302d5f9aebbdb
Bojan Mihaljevic <bmihaljevic@fi.upm.es>
2015-07-27 18:14:49 +0200

    adding release questions

--- R/release.R
+++ R/release.R
@@ -2,6 +2,8 @@ release_questions <- function() {
   c(
     "Have you checked all TODOs in code?",
     "Have you removed all commmented-out, not used code? Also in tests.",
-    "Have you checked on Travis?"
+    "Have you checked on Travis?",
+    "Does cran_comments.md show the notes from R-devel?",
+    "Does cran_comments.md specify the correct versions of R where tested?"
   )
 }
