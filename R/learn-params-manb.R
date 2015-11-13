86f9601f91035669ee633d1c78326f7d50509782
Bojan Mihaljevic <bmihaljevic@fi.upm.es>
2015-11-13 18:51:59 +0100

    consider prior in manb

--- R/learn-params-manb.R
+++ R/learn-params-manb.R
@@ -3,20 +3,20 @@
 # Formula for posterior assumes that smooth = 1. 
 #  @param manb_prior Currently ignored.
 # @return Returns a vector of probabilities of same length of ctgts. 
-compute_manb_arc_posteriors <- function(x, ctgts, smooth) {
+compute_manb_arc_posteriors <- function(x, ctgts, smooth, prior = 0.5) {
   if (!is_nb(x)) stop("MANB can only be applied to naive Bayes.")
   stopifnot(smooth > 0)
   stopifnot(is_subset(names(ctgts), features(x)))
   ctgts <- ctgts[features(x)]
-  vapply(ctgts, compute_manb_arc_posterior, smooth, FUN.VALUE = numeric(1))
+  vapply(ctgts, compute_manb_arc_posterior, smooth = smooth, prior = prior, 
+         FUN.VALUE = numeric(1))
 }
 # Assuming class is the last dimension.
 # Assuming that all instances are labelled
 # Returns a singe probability. 
-# TODO: Assuming an uniform structure prior.
-compute_manb_arc_posterior <- function(nijk, smooth) {
+compute_manb_arc_posterior <- function(nijk, smooth, prior) {
   #  If not 2D then it is not a naive Bayes
-  stopifnot(length(dim(nijk)) == 2)
+  stopifnot(length(dim(nijk)) == 2, are_probs(prior))
   r <- nrow(nijk)
   rt <- ncol(nijk)
   nij <- colSums(nijk)
@@ -25,7 +25,7 @@ compute_manb_arc_posterior <- function(nijk, smooth) {
   # P(D | C -> X)
   lpa <- rt * lgamma(r * smooth) - sum(lgamma(r * smooth + nij)) + sum(lgamma(smooth + nijk) - lgamma(smooth)) 
   # P(D | C ... X)
-  lpna <- lgamma(r * smooth) - lgamma(r * smooth + ni) + sum(lgamma(smooth + nik) - lgamma(smooth))  
+  lpna <- lgamma(r*smooth) - lgamma(r*smooth + ni) + sum(lgamma(smooth + nik) - lgamma(smooth))  
   # P(C -> X)
   denom <- matrixStats::logSumExp(c(lpna, lpa))
   lpa_post <- lpa - denom
@@ -34,15 +34,14 @@ compute_manb_arc_posterior <- function(nijk, smooth) {
   # Check valid probs
   stopifnot(all.equal(exp(lpa_post) + exp(lpna_post), 1))
   exp(lpa_post)
-  # Consider priors. 
-  # lpa_prior <- 6 * log(0.0001) 
-  # lnpa_prior <- 6 * log(1 - 0.0001) 
-  #   lpa_prior <- 6 * log(0.5) 
-  #   lnpa_prior <- 6 * log(1 - 0.5) 
-  #   lpa_num <- lpa + lpa_prior
-  #   lpna_num <- lpna + lnpa_prior
-  #   denom <- log_sum_exp(lpa_num, lpna_num)
-  #   lpa_post <- lpa_num - denom
-  #   lpna_post <- lpna_num - denom
-  #   exp(lpa_post) + exp(lpna_post)
+  # Consider priors: 
+  lpa_prior <- log(prior) 
+  lnpa_prior <- log(1 - prior) 
+  lpa_num <- lpa + lpa_prior
+  lpna_num <- lpna + lnpa_prior
+  denom <- matrixStats::logSumExp(c(lpa_num, lpna_num))
+  lpa_post <- lpa_num - denom
+  lpna_post <- lpna_num - denom
+  exp(lpa_post) + exp(lpna_post)
+  exp(lpa_post)
 }
--- tests/testthat/test-learn-params-manb.R
+++ tests/testthat/test-learn-params-manb.R
@@ -1,5 +1,7 @@
 context("learn params manb")
 
+# The exact posterior probabilities compare to were obtained using the MANB implementation by Wei et al. 
+
 test_that("compute manb nominal", {
   nb <- nbcar()
   a <- extract_ctgt(c('doors', 'class'), car)
@@ -7,7 +9,31 @@ test_that("compute manb nominal", {
   u <- lapply(vars, extract_ctgt, car)
   names(u) <- features(nb)
   d <- compute_manb_arc_posteriors(nb, u, smooth = 1)
-  expect_equivalent(d, c(1, 1, 0.000026294701543, 1, 1, 1))
+  expect_named(d, features(nb))
+  d <- as.vector(d)
+  expect_equal(d, c(1, 1, 0.000026294701543, 1, 1, 1))
+})
+
+test_that("compute manb no features", {
+  nb <- nbcarclass()
+  expect_equal(compute_manb_arc_posteriors(nb, list(), smooth = 1), numeric())
+})
+
+test_that("compute manb prior", {
+  d <- compute_manb_arc_posteriors(nb, u, smooth = 1, prior = 0.00001)
+  expect_named(d, features(nb))
+  d <- as.vector(d)
+  expect_equal(d, c(1, 1, 0.000000000262957, 1, 0.999981436585993, 1))
+  
+  d <- compute_manb_arc_posteriors(nb, u, smooth = 1, prior = 0.03)
+  expect_named(d, features(nb))
+  d <- as.vector(d)
+  expect_equal(d, c(1, 1, 0.000000813258915, 1, 0.999999993997562, 1))
+  
+  d <- compute_manb_arc_posteriors(nb, u, smooth = 1, prior = 0.95)
+  expect_named(d, features(nb))
+  d <- as.vector(d)
+  expect_equal(d, c(1, 1, 0.000499362978504, 1, 0.999999999990223, 1))
 })
 
 test_that("compute manb smooth", {
@@ -22,4 +48,12 @@ test_that("compute manb smooth", {
   d1 <- compute_manb_arc_posteriors(nb, u, smooth = 1)
   expect_true(sum(abs(d - d1)) < 1e-5)
   expect_error(compute_manb_arc_posteriors(nb, u, smooth = 0), " > 0 is not TRUE")
+})
+
+
+test_that("compute manb not nb", {
+  tn <- tan_cl('class', car)
+  ctgts <- families2ctgts(families(tn), car)[features(tn)]
+  expect_error(compute_manb(tn, ctgts, smooth = 1), 
+               "MANB can only be applied to naive Bayes")
 })
