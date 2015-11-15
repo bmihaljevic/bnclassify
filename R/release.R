b72eae8c8c206180f102657e813f01bbabb7afa3
Bojan Mihaljevic <bmihaljevic@fi.upm.es>
2015-11-15 11:33:52 +0100

    update release.R

--- R/release.R
+++ R/release.R
@@ -4,6 +4,7 @@ release_questions <- function() {
     "Have you removed all commmented-out, not used code? Also in tests.",
     "Have you checked on Travis?",
     "Does cran_comments.md show the notes from R-devel?",
-    "Does cran_comments.md specify the correct versions of R where tested?"
+    "Does cran_comments.md specify the correct versions of R where tested?",
+    "Is dont_run set for long-running examples?"
   )
 }
