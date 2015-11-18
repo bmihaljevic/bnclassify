41523766a8ef82e2a2eb02bd673296f3d0af828b
Bojan Mihaljevic <bmihaljevic@fi.upm.es>
2015-11-18 14:50:18 +0100

    fix docs predict. fix #13

--- R/predict.R
+++ R/predict.R
@@ -12,12 +12,18 @@
 #'@param ... Ignored.
 #'@return If \code{prob=FALSE}, then returns a length-\eqn{N} factor with the 
 #'  same levels as the class variable in \code{x}, where \eqn{N} is the number 
-#'  of rows in \code{newdata}. Each element of the factor is the most likely 
+#'  of rows in \code{newdata}. Each element is the most likely 
 #'  class for the corresponding row in \code{newdata}. If \code{prob=TRUE}, 
 #'  returns a \eqn{N} by \eqn{C} numeric matrix, where \eqn{C} is the number of 
 #'  classes; each row corresponds to the class posterior of the instance.
+#'  @examples 
+#'  data(car)
+#'  nb <- bnc('nb', 'class', car, smooth = 1)
+#'  p <- predict(nb, car)
+#'  head(p)
+#'  p <- predict(nb, car, prob = TRUE)
+#'  head(p)
 predict.bnc_bn <- function(object, newdata, prob = FALSE, ...) {      
-  # stopifnot(inherits(object, "bnc_fit"))  
   pred <- compute_cp(x = object, dataset = newdata)  
   if (!prob) {
     pred <- map(pred)
