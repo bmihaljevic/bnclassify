a34f45401db48af4021a76d9fcac2370b02e3fae
bmihaljevic <boki.mihaljevic@gmail.com>
2017-09-07 12:10:52 +0200

    add aode functions

--- R/learn-aode.r
+++ R/learn-aode.r
@@ -12,14 +12,15 @@ aode <- function(class_var, features, m = 1, ...) {
   models <- lapply(features, spode, features, class_var)
   bnc_aode_str(models = models, m = m, class_var = class_var)
 } 
-#' Is it en AODE?
-#' 
-#' @keywords internal
-is_aode <- function(x) {  
-  # if (!inherits(x, c('bnc_aode_str', 'bnc_aode'))) return (FALSE)    
-  # if (length(x$models) < 2) return (FALSE)  
-  # all(sapply(x$models, is_ode)) Should be is spode
-}  
+#' #' Is it en AODE?
+#' #' 
+#' #' @keywords internal
+#' is_aode <- function(x) {  
+#'   # if (!inherits(x, c('bnc_aode_str', 'bnc_aode'))) return (FALSE)    
+#'   # if (length(x$models) < 2) return (FALSE)  
+#'   # all(sapply(x$models, is_ode)) Should be is spode
+#'   return 
+#' }  
 #' Returns a Superparent one-dependence estimator. 
 #' 
 #' @param sp character The superparent.
@@ -30,6 +31,29 @@ spode <- function(sp, features, class) {
   stopifnot(class != sp)
   stopifnot(!class %in% features)
   features <- setdiff(features, sp)
-  features_graph <- nb_graph(root = sp, leaves = features)
-  superimpose_node(node = node, dag = features_graph)
+  features_graph <- nb_dag(class = sp, features = features)
+  superimpose_node(node = class, dag = features_graph)
+}
+#' Returns a \code{c("bnc_aode_str", "bnc")} object.
+#' @keywords internal
+bnc_aode_str <- function(models, m, class_var, log_struct = NULL) {
+  stopifnot(is.numeric(m))
+  stopifnot(length(models) > 0)
+  stopifnot(all(vapply(models, is_ode, FUN.VALUE = logical(1))))
+  features <- models[[1]]$features
+  bnc <- bnc_const(class_var = class_var, features = features, 
+                   log_struct = log_struct)
+  bnc$models <- models
+  bnc$m <- m
+  class(bnc) <- c('bnc_aode_str', class(bnc))
+  bnc
+}
+#' Fits an AODE model.
+#' @keywords internal
+bnc_aode <- function(x, fit_models, freqs) {
+  stopifnot(inherits(x, 'bnc_aode_str'))
+  x$models <- fit_models
+  x$freqs <- freqs
+  class(x) <- c('bnc_aode', 'bnc_fit', class(x))
+  x
 }
--- tests/testthat/test-learn-aode.r
+++ tests/testthat/test-learn-aode.r
@@ -1,9 +1,33 @@
 test_that("spode", {
   u <- spode(sp='D', features=LETTERS[5:9], class = 'C')
-  expect_equal(graph::numEdges(bnc_dag(u)), 11)
-  expect_equal(length(graph::adj(bnc_dag(u), 'D')$D), 5)
+  expect_equal(graph::numEdges(u), 11)
+  expect_equal(length(graph::adj(u, 'D')$D), 5)
   
   u <- spode(sp='E', features=LETTERS[c(4, 6:9)], class = 'C')
-  expect_equal(graph::numEdges(bnc_dag(u)), 11)
-  expect_equal(length(graph::adj(bnc_dag(u), 'E')$E), 5)
+  expect_equal(graph::numEdges(u), 11)
+  expect_equal(length(graph::adj(u, 'E')$E), 5)
 })
+
+test_that("aode str", {  
+  # with 1 feature is an nb
+  u <- aode(class = 'A', LETTERS[2])  
+  expect_true(is_ode(u))  
+  
+  u <- aode(class = 'A', LETTERS[2:10])  
+  expect_true(is_aode(u))    
+  expect_true(is_ode(u$models[['C']]))  
+  expect_equal(length(u$models), 9)
+  expect_identical(class_var(u), 'A')
+  expect_identical(features(u), LETTERS[2:10])
+  d <- u$models[[1]]
+  expect_equal(graph::numEdges(bnc_dag(d)), 9 + 8)
+  expect_equal(length(graph::adj(bnc_dag(d), 'B')$B), 8)
+  d <- u$models[[9]]
+  expect_equal(graph::numEdges(bnc_dag(d)), 9 + 8)
+  expect_equal(length(graph::adj(bnc_dag(d), 'J')$J), 8)
+})
+
+# test_that("fit aode and bnc", {
+#   a <- bnc(car, learner="aode")
+#   sapply(a$models, BIC, car) # No error 
+# })
