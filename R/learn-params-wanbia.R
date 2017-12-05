ee172d9d42556d4aa4056f10f2d6e0bc2e2d6112
bmihaljevic <boki.mihaljevic@gmail.com>
2017-12-05 11:27:13 +0100

    wanbia: minor refactor

--- R/learn-params-wanbia.R
+++ R/learn-params-wanbia.R
@@ -1,36 +1,34 @@
+# Using smooth = 1 by default
+fit_wanbia_nb <- function(class_var, dataset) { 
+  lp(nb(class_var, dataset), dataset, smooth = 1)
+}  
 #' Returns a function to compute negative conditional log-likelihood given feature weights
 #' @keywords internal
 make_cll <- function(class_var, dataset) {   
   check_class_in_dataset(class_var, dataset)
-  function(w) {  
-    nb <- make_weighted_nb(w, class_var, dataset)   
+  function(w) {   
+    nb <- fit_wanbia_nb(class_var, dataset)
+    nb <- set_weights(nb, w)
     - compute_cll(nb, dataset) 
   }    
-}
-fit_nb <- function(class_var, dataset) { 
-  lp(nb(class_var, dataset), dataset, smooth = 1)
 } 
-make_weighted_nb <- function(w, class_var, dataset) { 
-  nb <- fit_nb(class_var, dataset) 
-  nb <- set_weights(nb, setNames(w, features(nb) ))  
-  nb 
-}
 #' Returns a function to compute the gradient of negative conditional log-likelihood with respect to feature weights
 #' @keywords internal
 make_cll_gradient <- function(class_var, dataset) { 
  check_class_in_dataset(class_var, dataset)
  function(w) {  
-  weighted <- make_weighted_nb(w, class_var, dataset)  
-  unweighted <- fit_nb(class_var, dataset) 
-  compute_cll_gradients(weighted, unweighted, dataset)  
+  unweighted <- fit_wanbia_nb(class_var, dataset)
+  compute_cll_gradients(unweighted, dataset, w)  
  } 
 }  
-compute_cll_gradients <- function(weighted, unweighted, dataset) {
-  features <- features(weighted)
-  stopifnot(identical(features, features(unweighted)))
+compute_cll_gradients <- function(unweighted, dataset, w) {
+  stopifnot(is.null(unweighted$.weights))
+  features <- features(unweighted)
   db <- lapply(dataset, as.character)
   feats <- db[features]
   params <- params(unweighted)[features]
+  
+  weighted <- set_weights(unweighted, w)
   cp <- compute_cp(weighted, dataset = dataset)
   class_var <- class_var(weighted)
   grad <- mapply(cll_gradient_var, feats, params , MoreArgs = list(class = db[[class_var]], class_posterior = cp )) 
@@ -52,13 +50,16 @@ cll_gradient_var <- function(x, cpt, class, class_posterior) {
 #' @inheritParams nb
 #' @param class character 
 #' @param dataset The data frame from which to learn feature weights
+#' @param return_optim_object Return full output of `optim`
 #' @return a named numeric vector
-compute_wanbia_weights <- function(class, dataset) {
-  features <- setdiff(colnames(dataset), class)
+compute_wanbia_weights <- function(class, dataset, return_optim_object = FALSE) {
+  features <- get_features(class = class, dataset = dataset)
   # Initial weights are 1.
   w <- rep(1, length(features))
+  w <- setNames(w, features)
   cll <- make_cll(class, dataset) 
   cll_gradient <- make_cll_gradient(class, dataset) 
   w <- optim(w, cll, cll_gradient, method = 'L-BFGS-B', lower = 0, upper = 1)
-  setNames(w$par, features) 
+  if (return_optim_object ) return (w)
+  w$par
 }   
