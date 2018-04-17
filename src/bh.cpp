86accedb2ab94ce02bc7344170b60b3773ea8186
bmihaljevic <boki.mihaljevic@gmail.com>
2018-04-17 11:12:04 +0200

    inital boost make_graph

--- src/bh.cpp
+++ src/bh.cpp
@@ -7,117 +7,67 @@
 #include <boost/graph/directed_graph.hpp>
 
 /**
- * Basic adjacency list object api
- * Data: 
- *  mapping from names to ids.
- *  Matrix of edges between 
- * adj_list {
- *    string_vec nodes : all the string nodes I need. 
- *    edge matrix: uses ids as entries 
- *   public 
- *    int vec ids(): 
- *    edgeMatrix(names=FALSE): uses ids as entries 
- *    edgeMatrix(names=FALSE): uses names as entries 
- *    print(): print nicely
- * }     
- */
-
-/**
- * Remember that I cannot have the boost object in R. Thus I need to go back to R from boost. 
- * And the going back is either a matrix or an adj list
- */
-
-// TODO!!!!!!!!!!!! Not all vertices are in edges! Always add vertices!!!!!!!!!!!!!!!!! 
-
-// TODO!!!!!!!!!!!! I only need to call boost if I add types or not. 
-// ************** type bnc_dag_internal. **************  
-// So, my internal is just by 
+ * Boost uses integers as vertex ids, not names. 
+ * I cannot have the boost object in R, thus these functions just perform an operation and go back to the R structure. 
+ * And this is either a matrix or an adj list. 
+ */  
 
 using namespace boost; 
-using namespace Rcpp;    
+using namespace Rcpp;       
 
-/**
- * TODO: implement the interfac of dag.r. 
- */   
-
-/**
- * I wanted to use names as IDs but in bgl it is not possible. Those will have to be 
- */
-
+// The graph type I will use
 typedef adjacency_list<vecS, vecS, directedS> Graph;
 // typedef boost::directed_graph<> Graph;
 // this one did not work:
 // typedef adjacency_list<boost::directedS> Graph;
 
+// for connected components and such
+typedef adjacency_list <vecS, vecS, undirectedS> ugraph;
+
+// TODO: Currently not sure what this is doing.
 void print_vertices(Graph g) {
   typedef graph_traits<Graph>::vertex_iterator vertex_iterator;
   std::pair<vertex_iterator, vertex_iterator> viter;
   for (viter = vertices(g); viter.first != viter.second; ++viter.first) {
-    Rcout << *viter.first;
+    Rcout << *viter.first << std::endl;
   }
   Rcout << std::endl;
-}
-
+} 
 
 /**
- * Will do this first instead of a generic adj list -> graph. Will do that if required.
+ * This is an internal function.   
+ * Since not not all vertices need to be in edges, add vertices separately.
  */
-// [[Rcpp::export]]
-void make_graph(std::vector<std::string> vertices, Rcpp::IntegerMatrix edges)
+Graph make_graph(CharacterVector vertices, Rcpp::IntegerMatrix edges)
 {
-  // Initialize empty graph 
-  // Add any vertices
-  // Add any edges 
-  // Return graph. This is an internal function.  
-  // 
-  int n = vertices.size();
+  // any checks?
+  // Add vertices, if any 
+  int n = vertices.size(); 
   Graph g(n);
-  
-  typedef std::pair<int,int> Pair;
-  int nedges = edges.nrow();
-  std::vector<Pair> pairs;
-  pairs.reserve(nedges);
-  for (int i = 0; i < nedges; i++) { 
-    pairs.push_back(Pair(0,1));    
-    add_edge(0, 1, g);
+  for (int i = 0; i < n; i++) { 
+    add_vertex(g);
   } 
+  // Add edges, if any 
+  int nedges = edges.nrow();
+  for (int i = 0; i < nedges; i++) { 
+    add_edge(edges(i, 0), edges(i, 1), g);
+  }  
+  return g; 
+}
 
+// [[Rcpp::export]]  
+void test_make(CharacterVector vertices, Rcpp::IntegerMatrix edges) {
+  Graph g  = make_graph(vertices,  edges);
   print_vertices(g); 
-  // typedef property<first_name_t, std::string> FirstNameProperty;
-  // typedef adjacency_list<vecS, vecS, directedS,
-  //                        FirstNameProperty> MyGraphType;
-}
+}      
 
-
+// Requires an undirected graph 
 // [[Rcpp::export]]
-void make_nb2(std::string class_var, std::vector<std::string> features) { 
-  // typedef property<first_name_t, std::string> FirstNameProperty;
-  typedef adjacency_list<vecS, vecS, directedS> MyGraphType;
-  
-  typedef std::pair<int,int> Pair;
-  Pair edge_array[11] = { Pair(0,1), Pair(0,2), Pair(0,3), 
-                          Pair(0,4), Pair(2,0), Pair(3,0), 
-                          Pair(2,4), Pair(3,1), Pair(3,4), 
-                          Pair(4,0), Pair(4,1) };
-  
-  MyGraphType G(5);
-  for (int i = 0; i < 11; ++i)
-    add_edge(edge_array[i].first, edge_array[i].second, G);   
-}
-
-// [[Rcpp::export]]
-NumericVector bh_connected_comp(NumericVector x) 
-{
-  typedef adjacency_list <vecS, vecS, undirectedS> Graph;
-  
-  Graph G;
-  add_edge(0, 1, G);
-  add_edge(1, 4, G);
-  add_edge(4, 0, G);
-  add_edge(2, 5, G);
-  
-  std::vector<int> component(num_vertices(G));
-  int num = connected_components(G, &component[0]); 
+NumericVector bh_connected_comp(ugraph g) 
+{   
+  std::vector<int> component(num_vertices(g));
+  int num = connected_components(g, &component[0]); 
+  // TODO:: see additional checks from RBGL. Maybe connected comp might fail?
   std::vector<int>::size_type i;
   Rcout << "Total number of components: " << num << std::endl;  
   for (i = 0; i != component.size(); ++i)
@@ -126,8 +76,17 @@ NumericVector bh_connected_comp(NumericVector x)
   
  return wrap(component);
 }    
+
+// [[Rcpp::export]]
+void test_connected() {
+  ugraph g(3);
+  add_edge(0, 1, g); 
+  bh_connected_comp(g);
+} 
   
 /*** R
-make_nb('a', letters[1:5])
+dag <- tmp_nb_dag('a', letters[2:6])
+dag <- dag_internal2bgl(dag)
+test(dag$nodes, dag$edges)
 # For connected:  split(0:5, a)
 */
