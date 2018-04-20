f9300facce09a5249370f66127fd8636e298d371
bmihaljevic <boki.mihaljevic@gmail.com>
2018-04-20 19:38:18 +0200

    fix docs for check

--- R/wrap-graph.R
+++ R/wrap-graph.R
@@ -3,28 +3,35 @@
 # TODO: will use this function for conversion to graphNEL
 graphNEL2_graph_internal <- function(x) { 
   stopifnot(inherits(x, "graphNEL"))
-  nodes <- graph::nodes(x)
-  edges <- t(named_edge_matrix(x)) 
-  weights <- NULL 
-  tryCatch({
-  # Will fail if it does not have the attribute.
-   weights <- graph::edgeData(self = x, from = edges[, 1], to = edges[, 2], 
-                             attr = "weight")  
-  }, error = function(e) {}) 
-  weights <- unlist(weights)
-  edgemode <- graph::edgemode(x)
-  graph_internal(nodes, edges, weights, edgemode) 
+  if (requireNamespace("graph", quietly = TRUE)) {
+      nodes <- graph::nodes(x)
+      edges <- t(named_edge_matrix(x)) 
+      weights <- NULL 
+      tryCatch({
+      # Will fail if it does not have the attribute.
+       weights <- graph::edgeData(self = x, from = edges[, 1], to = edges[, 2], 
+                                 attr = "weight")  
+      }, error = function(e) {}) 
+      weights <- unlist(weights)
+      edgemode <- graph::edgemode(x)
+      graph_internal(nodes, edges, weights, edgemode) 
+  }
+  else stop()
 }  
 graph_internal2graph_NEL <- function(x) {  
-  stopifnot(inherits( x, "bnc_graph_internal")) 
-  edges <- graph_named_edge_matrix(x) 
-  weights <- x$weight
-  if (length(weights ) == 0) {
-    # ftM2graphNEL fails with a zero length W vector
-    weights <- NULL 
+  
+  if (!requireNamespace("graph", quietly = TRUE)) {
+    stop("Package graph required for this functionality.")
   }
-  # TODO: handle undirected. If directed, then build directed graph in BH.
-  graph::ftM2graphNEL(ft = edges, W = weights, V = x$nodes, edgemode = x$edgemode)  
+      stopifnot(inherits( x, "bnc_graph_internal")) 
+      edges <- graph_named_edge_matrix(x) 
+      weights <- x$weight
+      if (length(weights ) == 0) {
+        # ftM2graphNEL fails with a zero length W vector
+        weights <- NULL 
+      }
+      # TODO: handle undirected. If directed, then build directed graph in BH.
+      graph::ftM2graphNEL(ft = edges, W = weights, V = x$nodes, edgemode = x$edgemode)  
 }  
 # 
 #' Returns an edge matrix with node names (instead of node indices).
@@ -34,10 +41,13 @@ graph_internal2graph_NEL <- function(x) {
 #' @param g A graphNEL.
 #' @return A character matrix. 
 #' @keywords internal
-named_edge_matrix <- function(g) {
-  u <- graph::edgeMatrix(g)
-  u[] <- graph::nodes(g)[as.vector(u)]
-  if (length(u) == 0) mode(u) <- 'character'
-  stopifnot(is.character(u))
-  u
+named_edge_matrix <- function(g) { 
+  if (requireNamespace("graph", quietly = TRUE)) {
+      u <- graph::edgeMatrix(g)
+      u[] <- graph::nodes(g)[as.vector(u)]
+      if (length(u) == 0) mode(u) <- 'character'
+      stopifnot(is.character(u))
+      u 
+  }
+  else stop()
 }
