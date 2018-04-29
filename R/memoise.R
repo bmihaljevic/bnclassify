0e865a16b9d00e5e18131d10c6add4ddb7d1c345
Bojan Mihaljevic <bmihaljevic@fi.upm.es>
2018-04-29 13:20:58 +0200

    remove unneed check in memoise

--- R/memoise.R
+++ R/memoise.R
@@ -14,7 +14,7 @@ memoise_char <- memoize <- function(f) {
   cache <- new_cache()
   memo_f <- function(x) {
     hash <- paste0(x, collapse = ';')
-    stopifnot(length(hash) == 1L)
+    # stopifnot(length(hash) == 1L)
     if (cache$has_key(hash)) {
       cache$get(hash)
     } else {
