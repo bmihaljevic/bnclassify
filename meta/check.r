b6f3db93b5a32d6f230b29e936a36eb8e7b7d522
Bojan Mihaljevic <bmihaljevic@fi.upm.es>
2018-05-08 14:40:10 +0200

    avoid win builder warning with build arg

--- meta/check.r
+++ meta/check.r
@@ -1,12 +1,12 @@
 # Run checks prior to submitting 
 
 # First to build win so I can proceed with local while it is tested remotely 
-devtools::build_win('.', version = 'R-release')
-devtools::build_win('.', version = 'R-devel') 
 
-# for vignette size warning
-# devtools::check(args = '--as-cran', cran = TRUE, check_version = TRUE, build_args = c('--resave-data','--compact-vignettes="gs+qpdf"')) 
-devtools::check(cran = TRUE, check_version = TRUE, args = '--as-cran')
+# for vignette size warning. Since vignettes are only built locally, I only need add this argument
+devtools::build_win('.', version = 'R-release', args = c('--resave-data','--compact-vignettes="gs+qpdf"')) 
+devtools::build_win('.', version = 'R-devel', args = c('--resave-data','--compact-vignettes="gs+qpdf"')) ) 
+
+devtools::check(args = '--as-cran', cran = TRUE, check_version = TRUE, build_args = c('--resave-data','--compact-vignettes="gs+qpdf"'))
 # cran = FALSE probably runs tests skipped on cran
 devtools::check(cran = FALSE, check_version = TRUE, args = '--as-cran')
 devtools::check(cran = TRUE, check_version = TRUE )    
