bb2a9112d701c6a663cc81b405b9f6db10739fdc
Bojan Mihaljevic <bmihaljevic@fi.upm.es>
2018-05-09 16:17:52 +0200

    tmp fix in graph_remove_edges

--- R/graph-semi.r
+++ R/graph-semi.r
@@ -1,21 +1,21 @@
-
-# I could currently use undirected maybe
-
-
-# Semi directed graph with D and U edges
-## Data
-- The to - from matrix
-- The edge_mode vector of same length as matrix rows
-
-## Funcs
-- add edge. does not add node
-- add arc. does not add node 
-
-# Undirected
-
-## Funs
-- in_graph. used for discard reversed, for example, or similar.
-- compare_graphs
-
-- subgraph.
-- direct tree. This is a point here.
+# 
+# # I could currently use undirected maybe
+# 
+# 
+# # Semi directed graph with D and U edges
+# ## Data
+# - The to - from matrix
+# - The edge_mode vector of same length as matrix rows
+# 
+# ## Funcs
+# - add edge. does not add node
+# - add arc. does not add node 
+# 
+# # Undirected
+# 
+# ## Funs
+# - in_graph. used for discard reversed, for example, or similar.
+# - compare_graphs
+# 
+# - subgraph.
+# - direct tree. This is a point here.
