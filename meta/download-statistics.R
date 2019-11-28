ee733c6f4ee5fabe0c44838f6867a49e4bc46f3c
bmihaljevic <boki.mihaljevic@gmail.com>
2019-11-28 11:51:21 +0100

    update downloads dates

--- meta/download-statistics.R
+++ meta/download-statistics.R
@@ -1,9 +1,13 @@
 df <- cranlogs::cran_downloads('bnclassify', from = "2014-01-01", to = "2018-01-01")
 sum(df$count) 
 
-install.packages('adjustedcranlogs')
+
+df <- cranlogs::cran_downloads('bnclassify', from = "2014-01-01", to = "2019-09-25") 
+sum(df$count) 
+
+# install.packages('adjustedcranlogs')
 library(adjustedcranlogs)
-df <- adjustedcranlogs::adj_cran_downloads('bnclassify', from = "2014-01-01", to = "2018-01-01")
+df <- adjustedcranlogs::adj_cran_downloads('bnclassify', from = "2014-01-01", to = "2019-09-25")
 head(df)
 tail(df)
 plot(df$total_downloads)
