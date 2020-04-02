c91c7b99a79877c27350d383bd7d6536856127ef
Bojan Mihaljevic <boki.mihaljevic@gmail.com>
2020-04-02 16:17:15 +0200

    Revert "Merge pull request #36 from cheng270/master"
    This reverts commit d04969ddbce0afbed94f0f1955dc9748f71c7f8b, reversing
    changes made to 81cf9d5a08bda9061818302738e5e7f42c566379.

--- tests/testthat/test-anb-clg-params.R
+++
@@ -1,8 +0,0 @@
-context("filename: test_anb-clg-params")
-test_that("filename is test_anb-clg-params", {
-  #structure<-tan_cl('Species',as.data.frame(lapply(iris,as.factor)))
-  structure<-nb('Species',as.data.frame(lapply(iris,as.factor)))
-  plot(structure)
-  result<-lp_implement(structure,iris)
-  print(result)
-})
