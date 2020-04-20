c88f89286c77d205bd8df93f1177bb98a869eec4
Bojan Mihaljevic <boki.mihaljevic@gmail.com>
2020-04-20 12:09:32 +0200

    compare clg output to bnlearn

--- tests/testthat/test-anb-clg-params.R
+++ tests/testthat/test-anb-clg-params.R
@@ -15,3 +15,19 @@ test_that(
   }
 )
 
+
+test_that("compare coefficients and sds to bnlearn", { 
+    data(iris)
+    structure<-tan_cl('Species',as.data.frame(lapply(iris,as.factor)))
+    x<-BetaImplement(structure,iris)
+    coef <- x$Sepal.Length$coef
+    sd <- x$Sepal.Length$sd
+    
+    library(bnlearn)
+    a <- model2network(c("[Species][Sepal.Length|Species][Sepal.Width|Sepal.Length:Species][Petal.Length|Sepal.Length:Species][Petal.Width|Sepal.Length:Species]")) 
+    fit <- bn.fit(a, iris)
+    bncoef <- fit$Sepal.Length$coefficients
+    bnsd <- fit$Sepal.Length$sd 
+    expect_equal(coef, bncoef) 
+    expect_equal(sd, bnsd)   
+}) 
