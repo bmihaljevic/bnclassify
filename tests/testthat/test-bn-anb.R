7d681d520953a0b94998841ae51f14710c57f4c4
Bojan Mihaljevic <boki.mihaljevic@gmail.com>
2020-07-06 18:52:03 +0200

    add test: MI symmetry and non-negativity

--- tests/testthat/test-bn-anb.R
+++ tests/testthat/test-bn-anb.R
@@ -17,4 +17,19 @@ test_that("error with bic",{
 
 test_that("error with aic",{
   expect_error( bn_anb('Species', iris, score = 'bic'), "length(dm) >= 3L is not TRUE" )
+})  
+
+test_that("negative mutual information",{ 
+  mutual_information("Species", "Sepal.Length", iris)
+  mutual_information(c("Species", "Sepal.Width"), "Sepal.Length", iris)
+}) 
+
+test_that("non-symmetric mutual information",{ 
+  mutual_information("Species", "Sepal.Length", iris)
+  mutual_information("Sepal.Length", "Species",  iris)
 })
+
+test_that("family score becomes negative after adding a parent",{ 
+  family_scores("Species", "Sepal.Length", iris, score = 'loglik')
+  family_scores(c("Species", "Sepal.Width"), "Sepal.Length", iris, score = 'loglik')
+}) 
