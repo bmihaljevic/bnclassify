750a664fda68478e1aa369f8c7150dc765eedc0a
Bojan Mihaljevic <boki.mihaljevic@gmail.com>
2020-07-31 18:11:24 +0200

    plot multinet output

--- tests/testthat/test-multinet.R
+++ tests/testthat/test-multinet.R
@@ -2,13 +2,13 @@ context("inference")
 
 test_that("plot", {
   bn <- multinet_cl(class = 'class', dataset = car)
-  # Should be a message instead of an error
-  plot(bn) 
+  expect_output(plot(bn), "An ensemble of Bayesian network classifiers cannot be plotted")
 }) 
 
 test_that("Predict", { 
   bn <- multinet_cl(class = 'class', dataset = car)
   bn <- lp(bn, car, smooth = 0.1)
   a <- compute_cp(x = bn, car)
+  nparams(a)
 })  
 
