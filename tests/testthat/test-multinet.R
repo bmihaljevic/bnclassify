94fd69bf60ff22576028879885231420758e905b
Bojan Mihaljevic <boki.mihaljevic@gmail.com>
2020-07-31 18:26:36 +0200

    multinet: add accuracy and nparams tests

--- tests/testthat/test-multinet.R
+++ tests/testthat/test-multinet.R
@@ -9,6 +9,20 @@ test_that("Predict", {
   bn <- multinet_cl(class = 'class', dataset = car)
   bn <- lp(bn, car, smooth = 0.1)
   a <- compute_cp(x = bn, car)
-  nparams(a)
-})  
+})    
 
+test_that("Accuracy", { 
+  bn <- multinet_cl(class = 'class', dataset = car)
+  bn <- lp(bn, car, smooth = 0.1) 
+  p_bn <- predict(bn, car)
+  ac<-accuracy(p_bn, car$class)
+  expect_true(ac>0.9)
+})
+
+test_that("nparams not implemented", {  
+  bn <- multinet_cl(class = 'class', dataset = car)
+  bn <- lp(bn, car, smooth = 0.1)
+  # The number of parameters should be divided by r, where r is the number of classes, 
+  # since each local model has r-1 dummy classes
+  nparams(bn)
+})
