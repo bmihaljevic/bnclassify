936a38dd119875162b50c19f8138d39376129239
bmihaljevic <boki.mihaljevic@gmail.com>
2024-03-12 11:59:28 +0100

    minor igraph weights fix

--- R/wrap-igraph.R
+++ R/wrap-igraph.R
@@ -1,40 +1,28 @@
-# TODO: requiere message for igraph package
-graphNEL2_graph_internal <- function(x) { 
+graphNEL2_graph_internal <- function(x) {
   stopifnot(inherits(x, "igraph"))
-  if (requireNamespace("igraph", quietly = TRUE)) {
-      nodes <- igraph::V(x)$name
-      edges <- igraph::as_edgelist(x)  # Assuming 'x' is an igraph object
-      colnames(edges) <- c("from", "to")
-      # edges <- t(named_edge_matrix(x)) 
-      weights <- NULL
-      tryCatch({
-        # Will fail if it does not have the attribute.
-        weights <- igraph::E(x)$weight
-      }, error = function(e) {})
-      edgemode <- ifelse(igraph::is_directed(x), "directed", "undirected")
-      weights <- igraph::E(x)$weight
-      graph_internal(nodes, edges, weights, edgemode) 
-  }
-  else stop()
+  nodes <- igraph::V(x)$name
+  edges <- igraph::as_edgelist(x)  # Assuming 'x' is an igraph object
+  colnames(edges) <- c("from", "to")
+  # edges <- t(named_edge_matrix(x)) 
+  weights <- NULL
+  tryCatch({
+    # Will fail if it does not have the attribute.
+    weights <- igraph::E(x)$weight
+  }, error = function(e) {})
+  edgemode <- ifelse(igraph::is_directed(x), "directed", "undirected")
+  weights <- igraph::E(x)$weight
+  graph_internal(nodes, edges, weights, edgemode) 
 }  
-graph_internal2graph_NEL <- function(x) {  
-  if (!requireNamespace("igraph", quietly = TRUE)) {
-    stop("Package igraph required for this functionality.")
-  }
-      stopifnot(inherits( x, "bnc_graph_internal")) 
-      edges <- x$edges 
-      weights <- x$weight
-      if (length(weights ) == 0) {
-        # ftM2graphNEL fails with a zero length W vector
-        weights <- NULL 
-      }
-      # TODO: handle undirected. If directed, then build directed graph in BH.
-      # graph::ftM2graphNEL(ft = edges, W = weights, V = x$nodes, edgemode = x$edgemode)  
-      edgemode <- ifelse(x$edgemode == "directed", TRUE, FALSE)
-      
-      # Build the igraph graph
-      graph <- igraph::graph_from_edgelist(edges, directed = edgemode)
-      # igraph::E(graph)$weight <- weights
+graph_internal2graph_NEL <- function(x) {
+    stopifnot(inherits( x, "bnc_graph_internal")) 
+    edges <- x$edges 
+    edgemode <- ifelse(x$edgemode == "directed", TRUE, FALSE)
+    graph <- igraph::graph_from_edgelist(edges, directed = edgemode)
+    weights <- x$weight
+    if (length(weights ) > 0) {
+      stopifnot(length(weights ) == length(igraph::E(graph)))
       igraph::set_edge_attr(graph, "weight", value = weights)
-      graph 
+    }
+    # TODO: handle undirected. If directed, then build directed graph in BH.
+    graph 
 }  
